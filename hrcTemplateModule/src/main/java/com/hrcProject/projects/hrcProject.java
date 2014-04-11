0c48695f7f9e72329170691bb028cd1ddf94d2a3
bayois <bayois@gmai.com>
2014-04-11 17:28:57 +0200

    First Commit

---
+++ hrcTemplateModule/src/main/java/com/hrcProject/projects/hrcProject.java
@@ -0,0 +1,12 @@
+package com.hrcProject.projects;
+
+/**
+ * This class is optional and represents the configuration for the hrcTemplateModule module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/hrcTemplateModule</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ */
+public class hrcProject {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
